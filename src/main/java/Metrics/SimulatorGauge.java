2e1c470fc650ec05c3ad8feda8d29078937b6add
yhassanzadeh13@ku.edu.tr <yhassanzadeh13@ku.edu.tr>
2021-06-12 10:31:10 -0700

    reorganizes simulation gauge

--- src/main/java/Metrics/SimulatorGauge.java
+++ src/main/java/Metrics/SimulatorGauge.java
@@ -10,8 +10,29 @@ import java.util.UUID;
  * For every registered metric, the metric will be collected for each node separately
  * Gauges can be incremented and decremented.
  */
-
 public class SimulatorGauge extends SimulatorMetric implements GaugeCollector {
+
+    /**
+     * Register a new metric with a specific name
+     *
+     * @param name
+     * @return True in case of success
+     */
+    @Override
+    public boolean register(String name) {
+        if (!collectors.containsKey(name)) {
+            collectors.put(name, Gauge.build().namespace(NAMESPACE).name(name).help(HELP_MSG).labelNames(LABEL_NAME).register());
+            collectorsTypes.put(name, TYPE.GAUGE);
+        }
+        else {
+            if (collectorsTypes.get(name) != TYPE.GAUGE) {
+                System.err.println("[SimulatorGauge] Collector name is already registered with a different type " + collectorsTypes.get(name));
+                Simulator.getLogger().error("[SimulatorGauge] Collector name is already registered with a different type " + collectorsTypes.get(name));
+                return false;
+            }
+        }
+        return true;
+    }
 
     @Override
     public boolean inc(String name, UUID id) {
@@ -73,28 +94,6 @@ public class SimulatorGauge extends SimulatorMetric implements GaugeCollector {
             return null;
         }
         return (Gauge) collectors.get(name);
-    }
-
-    /**
-     * Register a new metric with a specific name
-     *
-     * @param name
-     * @return True in case of success
-     */
-    @Override
-    public boolean register(String name) {
-        if (!collectors.containsKey(name)) {
-            collectors.put(name, Gauge.build().namespace(NAMESPACE).name(name).help(HELP_MSG).labelNames(LABEL_NAME).register());
-            collectorsTypes.put(name, TYPE.GAUGE);
-        }
-        else {
-            if (collectorsTypes.get(name) != TYPE.GAUGE) {
-                System.err.println("[SimulatorGauge] Collector name is already registered with a different type " + collectorsTypes.get(name));
-                Simulator.getLogger().error("[SimulatorGauge] Collector name is already registered with a different type " + collectorsTypes.get(name));
-                return false;
-            }
-        }
-        return true;
     }
 
 }
