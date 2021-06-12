f94086c90150a800302eb03a271b6127e678816a
yhassanzadeh13@ku.edu.tr <yhassanzadeh13@ku.edu.tr>
2021-06-12 10:50:49 -0700

    reorganizes metrics and removes their abstract definition

--- src/main/java/Metrics/SimulatorCollector.java
+++ src/main/java/Metrics/SimulatorCollector.java
@@ -1,2 +1,28 @@
-package Metrics;public class PromCollector {
+package Metrics;
+
+public class SimulatorCollector implements MetricsCollector {
+    private final SimulatorCounter mCounter;
+    private final SimulatorGauge mGauge;
+    private final SimulatorHistogram mHistogram;
+
+    SimulatorCollector() {
+        this.mCounter = new SimulatorCounter();
+        this.mGauge = new SimulatorGauge();
+        this.mHistogram = new SimulatorHistogram();
+    }
+
+    @Override
+    public HistogramCollector getHistogramCollector() {
+        return this.mHistogram;
+    }
+
+    @Override
+    public GaugeCollector getGaugeCollector() {
+        return this.mGauge;
+    }
+
+    @Override
+    public CounterCollector getCounterCollector() {
+        return this.mCounter;
+    }
 }
