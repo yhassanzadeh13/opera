cc98bc97efbd0800fb772c4cbc35052e50de9fed
yhassanzadeh13 <yhassanzadeh13@ku.edu.tr>
2021-08-21 10:17:43 -0700

    fixes lint

--- src/main/java/Underlay/UnderlayFactory.java
+++ src/main/java/Underlay/UnderlayFactory.java
@@ -13,26 +13,24 @@ import underlay.tcp.TcpUnderlay;
 import underlay.udp.UdpUnderlay;
 
 /**
- *UnderlayFactory is a factory which consists of Underlays.
+ * UnderlayFactory is a factory which consists of Underlays.
  */
 public class UnderlayFactory {
+
+  private static final Logger log = Logger.getLogger(UnderlayFactory.class.getName());
+  // hashmap to hold the underlay types to underlay class names
+  private static HashMap<String, String> underlayClassName;
+  private static final String yamlFile = "./src/main/java/underlay/underlayTypes.yml";
 
   private UnderlayFactory() {
   }
 
-  private static Logger log = Logger.getLogger(UnderlayFactory.class.getName());
-
-  // hashmap to hold the underlay types to underlay class names
-  private static HashMap<String, String> underlayClassName;
-
-  private static String yamlFile = "./src/main/java/underlay/underlayTypes.yml";
-
   /**
    * getter of a mock Underlay.
    *
-   * @param address address of the underlay
-   * @param port port of the underlay
-   * @param middleLayer middle layer of the underlay
+   * @param address          address of the underlay
+   * @param port             port of the underlay
+   * @param middleLayer      middle layer of the underlay
    * @param allLocalUnderlay hashmap of all underlays
    * @return underlay
    */
@@ -49,13 +47,13 @@ public class UnderlayFactory {
    * get a new underlay instance.
    *
    * @param underlayName the underlay type name according to underlayTypes yaml file
-   * @param port port of the underlay
-   * @param middleLayer middle layer of the underlay
+   * @param port         port of the underlay
+   * @param middleLayer  middle layer of the underlay
    * @return new underlay instance according to the given type
    */
   public static Underlay newUnderlay(UnderlayType underlayName, int port, MiddleLayer middleLayer) {
     Underlay underlay;
-    switch (underlayName){
+    switch (underlayName) {
       case JAVA_RMI:
         underlay = new JavaRmiUnderlay();
         break;
