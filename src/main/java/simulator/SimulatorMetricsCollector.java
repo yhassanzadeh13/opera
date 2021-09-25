7c4f809ad79db07b58ed99dc31d8bf757b328024
yhassanzadeh13 <yhassanzadeh13@ku.edu.tr>
2021-09-25 07:12:58 -0700

    adds constants to simulator metrics

--- src/main/java/simulator/SimulatorMetricsCollector.java
+++ src/main/java/simulator/SimulatorMetricsCollector.java
@@ -1,2 +1,20 @@
-package simulator;public class SimulatorMetricsCollector {
+package simulator;
+
+
+import metrics.MetricsCollector;
+
+public class SimulatorMetricsCollector {
+  public class Name {
+    public static final String SESSION_LENGTH = "SessionLength";
+    public static final String INTER_ARRIVAL = "InterArrival";
+  }
+
+  public class HelpMsg {
+    public static final String SESSION_LENGTH = "session length of nodes based on churn distribution";
+    public static final String INTER_ARRIVAL = "inter arrival time of nodes based on churn distribution";
+  }
+
+  public SimulatorMetricsCollector(MetricsCollector metricsCollector) {
+
+  }
 }
