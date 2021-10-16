cd7b7a53a7b3d78221760ee25aa22dc0b0226103
yhassanzadeh13 <yhassanzadeh13@ku.edu.tr>
2021-10-16 06:18:27 -0700

    fixes lints in lightchain metrics

--- src/main/java/scenario/pov/LightChainMetrics.java
+++ src/main/java/scenario/pov/LightChainMetrics.java
@@ -8,22 +8,31 @@ import java.util.concurrent.locks.ReentrantLock;
 import metrics.Constants;
 import metrics.MetricsCollector;
 
+/**
+ * Metrics collector for LightChain simulations.
+ */
 public class LightChainMetrics {
+  private static final ReentrantLock lock = new ReentrantLock();
+  private static final String SUBSYSTEM_LIGHTCHAIN = "lightchain";
   private static MetricsCollector metricsCollector;
   private static UUID collectorID;
   private static HashMap<Integer, List<UUID>> blockInventory;
-  private static ReentrantLock initLock = new ReentrantLock();
 
-  /***
+  /**
    * Initializes LightChainMetrics collector.
+   *
    * @param metricsCollector an instance of metric collector. Supposed to be the same instance over all invocations by
    *                         different nodes. Though only the first invocation goes through.
    */
   public LightChainMetrics(MetricsCollector metricsCollector) {
-    initLock.lock();
+    if (!lock.tryLock()) {
+      // another thread is initiating
+      return;
+    }
 
-    if(blockInventory != null){
+    if (blockInventory != null) {
       // already initialized
+      lock.unlock();
       return;
     }
 
@@ -39,47 +48,82 @@ public class LightChainMetrics {
     collectorID = UUID.randomUUID();
 
     LightChainMetrics.metricsCollector.counter().register(
-        Constants.Demo.LightChain.Name.TRANSACTION_COUNT,
+        LightChain.Name.TRANSACTION_COUNT,
         Constants.Namespace.DEMO,
-        Constants.Demo.Subsystem.LightChain,
-        Constants.Demo.LightChain.HelpMsg.TRANSACTION_COUNT
+        SUBSYSTEM_LIGHTCHAIN,
+        LightChain.HelpMsg.TRANSACTION_COUNT
     );
     LightChainMetrics.metricsCollector.counter().register(
-        Constants.Demo.LightChain.Name.TOTAL_BLOCKS_COUNT,
+        LightChain.Name.TOTAL_BLOCKS_COUNT,
         Constants.Namespace.DEMO,
-        Constants.Demo.Subsystem.LightChain,
-        Constants.Demo.LightChain.HelpMsg.TOTAL_BLOCKS_COUNT
+        SUBSYSTEM_LIGHTCHAIN,
+        LightChain.HelpMsg.TOTAL_BLOCKS_COUNT
     );
     LightChainMetrics.metricsCollector.counter().register(
-        Constants.Demo.LightChain.Name.TOTAL_UNIQUE_BLOCKS_COUNT,
+        LightChain.Name.TOTAL_UNIQUE_BLOCKS_COUNT,
         Constants.Namespace.DEMO,
-        Constants.Demo.Subsystem.LightChain,
-        Constants.Demo.LightChain.HelpMsg.TOTAL_UNIQUE_BLOCKS_COUNT
+        SUBSYSTEM_LIGHTCHAIN,
+        LightChain.HelpMsg.TOTAL_UNIQUE_BLOCKS_COUNT
     );
     LightChainMetrics.metricsCollector.gauge().register(
-        Constants.Demo.LightChain.Name.CURRENT_BLOCK_HEIGHT,
+        LightChain.Name.CURRENT_BLOCK_HEIGHT,
         Constants.Namespace.DEMO,
-        Constants.Demo.Subsystem.LightChain,
-        Constants.Demo.LightChain.HelpMsg.CURRENT_BLOCK_HEIGHT
+        SUBSYSTEM_LIGHTCHAIN,
+        LightChain.HelpMsg.CURRENT_BLOCK_HEIGHT
     );
 
-    initLock.unlock();
+    lock.unlock();
   }
 
   /**
    * Is invoked whenever there is a new transaction created by a node, and increases the total number of transactions.
+   *
    * @param count number of transactions created.
    */
-  public void OnNewTransactions(int count) {
-    metricsCollector.counter().inc(Constants.Demo.LightChain.Name.TRANSACTION_COUNT, collectorID, count);
+  public void onNewTransactions(int count) {
+    metricsCollector.counter().inc(LightChain.Name.TRANSACTION_COUNT, collectorID, count);
   }
 
   /**
    * OnNewBlock is invoked whenever a node creates a new finalized block, i.e., the node is the owner of that block. It
    * increments the total number of finalized blocks in the system.
    */
-  private void OnNewFinalizedBlock() {
-    metricsCollector.counter().inc(Constants.Demo.LightChain.Name.TOTAL_BLOCKS_COUNT, collectorID);
+  private void onNewFinalizedBlock() {
+    metricsCollector.counter().inc(LightChain.Name.TOTAL_BLOCKS_COUNT, collectorID);
+  }
+
+  /**
+   * OnNewFinalizedBlock is invoked whenever a node creates a finalized block.
+   * It keeps track of the total blocks as well as total unique block heights finalized in the system.
+   * It also updates the telemetry view of node towards current finalized block height.
+   *
+   * @param blockHeight new finalized block height node generated.
+   * @param blockId     identifier of the newly generated finalized block.
+   * @param nodeId      identifier of node generating finalized block height.
+   */
+  public void onNewFinalizedBlock(int blockHeight, UUID blockId, UUID nodeId) {
+    onBlockHeightUpdated(blockHeight, nodeId);
+
+    if (!blockInventory.containsKey(blockHeight)) {
+      // there is no other block at this height yet
+      // block is unique at this height
+      blockInventory.put(blockHeight, new ArrayList<>(List.of(blockId)));
+      this.onNewFinalizedBlock();
+      this.onNewUniqueBlock();
+
+      return;
+    }
+
+    List<UUID> blockIdsAtHeight = blockInventory.get(blockHeight);
+    if (blockIdsAtHeight.contains(blockId)) {
+      // duplicate block
+      return;
+    }
+
+    blockIdsAtHeight.add(blockId);
+    blockInventory.put(blockHeight, blockIdsAtHeight);
+    // block is not unique at this height
+    this.onNewFinalizedBlock();
   }
 
   /**
@@ -87,52 +131,37 @@ public class LightChainMetrics {
    * the same height. It increments total number of unique blocks in the system. Note that unique blocks is an ephemeral
    * concept as other blocks at the same height may be finalized.
    */
-  private void OnNewUniqueBlock() {
-    metricsCollector.counter().inc(Constants.Demo.LightChain.Name.TOTAL_UNIQUE_BLOCKS_COUNT, collectorID);
+  private void onNewUniqueBlock() {
+    metricsCollector.counter().inc(LightChain.Name.TOTAL_UNIQUE_BLOCKS_COUNT, collectorID);
   }
 
   /**
    * OnBlockHeightUpdated is invoked whenever the finalized block height on a node is progressing, i.e.,
    * by creating new finalized blocks.
    * It keeps track of the block height update per individual node.
+   *
    * @param blockHeight new generated finalized block height
-   * @param nodeID the node identifier that generated the finalized block.
+   * @param nodeId      the node identifier that generated the finalized block.
    */
-  private void OnBlockHeightUpdated(int blockHeight, UUID nodeID){
-    metricsCollector.gauge().set(Constants.Demo.LightChain.Name.CURRENT_BLOCK_HEIGHT, nodeID, blockHeight);
+  private void onBlockHeightUpdated(int blockHeight, UUID nodeId) {
+    metricsCollector.gauge().set(LightChain.Name.CURRENT_BLOCK_HEIGHT, nodeId, blockHeight);
   }
 
-  /**
-   * OnNewFinalizedBlock is invoked whenever a node creates a finalized block. It keeps track of the total blocks as well
-   * as total unique block heights finalized in the system. It also updates the telemetry view of node towards current finalized
-   * block height.
-   * @param blockHeight new finalized block height node generated.
-   * @param blockID identifier of the newly generated finalized block.
-   * @param nodeID identifier of node generating finalized block height.
-   */
-  public void OnNewFinalizedBlock(int blockHeight, UUID blockID, UUID nodeID) {
-    OnBlockHeightUpdated(blockHeight, nodeID);
-
-    if (!blockInventory.containsKey(blockHeight)) {
-      // there is no other block at this height yet
-      // block is unique at this height
-      blockInventory.put(blockHeight, new ArrayList<>(List.of(blockID)));
-      this.OnNewFinalizedBlock();
-      this.OnNewUniqueBlock();
-
-      return;
+  private static class LightChain {
+    public class Name {
+      public static final String TRANSACTION_COUNT = "transaction_count";
+      public static final String CURRENT_BLOCK_HEIGHT = "block_height";
+      public static final String TOTAL_BLOCKS_COUNT = "total_finalized_blocks";
+      public static final String TOTAL_UNIQUE_BLOCKS_COUNT = "total_unique_finalized_blocks";
     }
 
-    List<UUID> blockIDsAtHeight = blockInventory.get(blockHeight);
-    if (blockIDsAtHeight.contains(blockID)) {
-      // duplicate block
-      return;
+    public class HelpMsg {
+      public static final String TRANSACTION_COUNT = "total number of transactions made in system";
+      public static final String CURRENT_BLOCK_HEIGHT = "last finalized block height";
+      public static final String TOTAL_BLOCKS_COUNT = "total finalized blocks";
+      public static final String TOTAL_UNIQUE_BLOCKS_COUNT = "total unique finalized blocks";
     }
-
-    blockIDsAtHeight.add(blockID);
-    blockInventory.put(blockHeight, blockIDsAtHeight);
-    // block is not unique at this height
-    this.OnNewFinalizedBlock();
   }
+
 
 }
