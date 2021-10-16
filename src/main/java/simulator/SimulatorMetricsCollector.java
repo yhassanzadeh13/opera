4824b4f5925842fccc0ffb02e9953f9295a9a783
yhassanzadeh13 <yhassanzadeh13@ku.edu.tr>
2021-10-16 06:17:41 -0700

    fixes lint for simulator metrics collector

--- src/main/java/simulator/SimulatorMetricsCollector.java
+++ src/main/java/simulator/SimulatorMetricsCollector.java
@@ -3,25 +3,22 @@ package simulator;
 
 import java.util.UUID;
 import metrics.Constants;
-import metrics.Metrics;
 import metrics.MetricsCollector;
 
+/**
+ * SimulatorMetricsCollector is the metrics collector for the core simulator's functionalities.
+ */
 public class SimulatorMetricsCollector {
   private static final String SUBSYSTEM_CHURN = "churn";
   private static final String NAMESPACE_SIMULATOR = "simulator";
-  private static class Name {
-    public static final String SESSION_LENGTH = "session_length";
-    public static final String INTER_ARRIVAL = "inter_arrival";
-  }
-
-  private static class HelpMsg {
-    public static final String SESSION_LENGTH = "session length of nodes based on churn distribution";
-    public static final String INTER_ARRIVAL = "inter arrival time of nodes based on churn distribution";
-  }
-
-  private static MetricsCollector metricsCollector;
   private static final UUID collectorID = UUID.randomUUID();
+  private static MetricsCollector metricsCollector;
 
+  /**
+   * Creates a metric collector for core simulator functionalities.
+   *
+   * @param metricsCollector root metric collector of opera.
+   */
   public SimulatorMetricsCollector(MetricsCollector metricsCollector) {
     SimulatorMetricsCollector.metricsCollector = metricsCollector;
 
@@ -44,10 +41,11 @@ public class SimulatorMetricsCollector {
   /**
    * Records the generated session length of the node in histogram. Session length is the online
    * duration of a node in the system.
-   * @param id identifier of node.
+   *
+   * @param id            identifier of node.
    * @param sessionLength its session length.
    */
-  public void OnNewSessionLengthGenerated(UUID id, int sessionLength){
+  public void onNewSessionLengthGenerated(UUID id, int sessionLength) {
     metricsCollector.histogram().observe(Name.SESSION_LENGTH, id, sessionLength);
   }
 
@@ -55,11 +53,22 @@ public class SimulatorMetricsCollector {
    * Records the generated inter-arrival time of the node in histogram. Inter arrival time
    * is a global parameter of simulation denoting the time between two consecutive arrival of
    * nodes to the system.
+   *
    * @param interArrival its inter arrival time.
    */
-  public void OnNewInterArrivalGenerated(int interArrival){
+  public void onNewInterArrivalGenerated(int interArrival) {
     // Since inter arrival time is a global parameter, we record it by the collector id which
     // is a global identifier.
     metricsCollector.histogram().observe(Name.INTER_ARRIVAL, collectorID, interArrival);
+  }
+
+  private static class Name {
+    public static final String SESSION_LENGTH = "session_length";
+    public static final String INTER_ARRIVAL = "inter_arrival";
+  }
+
+  private static class HelpMsg {
+    public static final String SESSION_LENGTH = "session length of nodes based on churn distribution";
+    public static final String INTER_ARRIVAL = "inter arrival time of nodes based on churn distribution";
   }
 }
