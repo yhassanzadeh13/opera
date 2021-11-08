aab06da779e040c227d425454eef71a8c3553799
yhassanzadeh13 <yhassanzadeh13@ku.edu.tr>
2021-11-08 06:00:57 -0800

    fixes lint issues in constants

--- src/main/java/metrics/Constants.java
+++ src/main/java/metrics/Constants.java
@@ -1,21 +1,26 @@
 package metrics;
 
+/**
+ * Global constants used across codebase.
+ */
 public class Constants {
 
   public static final String UUID = "uuid";
 
-  public class Namespace {
+  /**
+   * General namespaces used for metric collection. By the general we mean those
+   * shared among at least two classes.
+   */
+  public static class Namespace {
     public static final String NETWORK = "network";
     public static final String DEMO = "demo";
   }
 
 
-
-
   /**
    * Contains default constant values for histogram collectors.
    */
-  public class Histogram {
+  public static class Histogram {
     public static final double[] DEFAULT_HISTOGRAM = new double[]{Double.MAX_VALUE};
   }
 }
