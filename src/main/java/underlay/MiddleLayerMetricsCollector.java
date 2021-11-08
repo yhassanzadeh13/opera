0c650e72ebe322360c0e1c8762504b41f2f8a3f8
yhassanzadeh13 <yhassanzadeh13@ku.edu.tr>
2021-11-08 05:57:36 -0800

    fixes lint issues in middlelayer

--- src/main/java/underlay/MiddleLayerMetricsCollector.java
+++ src/main/java/underlay/MiddleLayerMetricsCollector.java
@@ -6,19 +6,26 @@ import metrics.Constants;
 import metrics.MetricsCollector;
 import utils.SimulatorUtils;
 
+/**
+ * Encapsulates metrics collectors for middlelayer of network.
+ */
 public class MiddleLayerMetricsCollector {
   public static final String SUBSYSTEM_MIDDLELAYER = "middlelayer";
   private static final ReentrantLock lock = new ReentrantLock();
-  public static final String PACKET_SIZE = "packet_size";
   private static MetricsCollector metricsCollector;
 
+  /**
+   * Atomically initiates metric collector for middlelayer exactly once.
+   *
+   * @param metricsCollector root metric collector.
+   */
   public MiddleLayerMetricsCollector(MetricsCollector metricsCollector) {
     if (!lock.tryLock()) {
       // another thread is initiating
       return;
     }
 
-    if (MiddleLayerMetricsCollector.metricsCollector != null){
+    if (MiddleLayerMetricsCollector.metricsCollector != null) {
       // already initialized
       lock.unlock();
       return;
@@ -63,32 +70,36 @@ public class MiddleLayerMetricsCollector {
    * onMessageReceived is called whenever a new message received by a node.
    * It increments number of messages received by this node, as well as stops the timer for propagation delay.
    * It also records size of the message in bytes.
+   *
    * @param receiverId identifier of receiver.
-   * @param senderId identifier of sender.
-   * @param size size of message in bytes.
+   * @param senderId   identifier of sender.
+   * @param size       size of message in bytes.
    */
-  public void onMessageReceived(UUID receiverId, UUID senderId, int size){
+  public void onMessageReceived(UUID receiverId, UUID senderId, int size) {
     MiddleLayerMetricsCollector.metricsCollector.counter().inc(Name.MESSAGE_RECEIVED_TOTAL, receiverId);
-    MiddleLayerMetricsCollector.metricsCollector.histogram().tryObserveDuration(Name.PROPAGATION_DELAY, delayBucketHash(senderId, receiverId));
+    MiddleLayerMetricsCollector.metricsCollector.histogram()
+        .tryObserveDuration(Name.PROPAGATION_DELAY, delayBucketHash(senderId, receiverId));
     MiddleLayerMetricsCollector.metricsCollector.histogram().observe(Name.PACKET_SIZE, senderId, size);
   }
 
   /**
    * onMessageSent is called whenever a new message is sent by a node.
    * It increments the number of messages sent, as well as starts a timer for propagation delay.
-   * @param senderId identifier of sender.
+   *
+   * @param senderId   identifier of sender.
    * @param receiverId identifier of receiver.
    */
-  public void onMessageSent(UUID senderId, UUID receiverId){
+  public void onMessageSent(UUID senderId, UUID receiverId) {
     MiddleLayerMetricsCollector.metricsCollector.counter().inc(Name.MESSAGE_SENT_TOTAL, senderId);
-    MiddleLayerMetricsCollector.metricsCollector.histogram().
-        startTimer(Name.PROPAGATION_DELAY, senderId, delayBucketHash(senderId, receiverId));
+    MiddleLayerMetricsCollector.metricsCollector.histogram()
+        .startTimer(Name.PROPAGATION_DELAY, senderId, delayBucketHash(senderId, receiverId));
   }
 
   /**
    * Uniquely hashes the pair of (senderId, receiverId) into a hash value that is used to keep track of
    * propagation delay.
-   * @param senderId identifier of sender.
+   *
+   * @param senderId   identifier of sender.
    * @param receiverId identifier of receiver.
    * @return hash value of the (senderId, receiverId)
    */
