a01c078ab4ccf4e71fba65046fbe1e77707d1a3f
yhassanzadeh13 <yhassanzadeh13@ku.edu.tr>
2021-11-12 06:20:33 -0800

    extracts single node test into a separate test

--- src/test/java/metrics/SimulatorGaugeTest.java
+++ src/test/java/metrics/SimulatorGaugeTest.java
@@ -12,43 +12,63 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class SimulatorGaugeTest {
-  static final int THREAD_CNT = 50;
-  static final int ITERATIONS = 2000;
-  static final double EPS = 0.01;
-  static JDKRandomGenerator rand = new JDKRandomGenerator();
-  CountDownLatch count;
+  private static final String TEST_GAUGE = "test_gauge";
+  private static final String SUBSYSTEM_GAUGE_TEST = "subsystem_gauge_test";
+  private static final int THREAD_CNT = 50;
+  private static final int ITERATIONS = 2000;
+  private static final double EPS = 0.01;
+  private static final JDKRandomGenerator rand = new JDKRandomGenerator();
+  private CountDownLatch count;
   private MetricsCollector metricsCollector;
 
 
   @BeforeEach
   void setup() {
     metricsCollector = new OperaCollector();
-  }
-
-  @Test
-  void valueTest() {
-    final String TEST_GAUGE = "test_gauge";
-    final String SUBSYSTEM_GAUGE_TEST = "subsystem_gauge_test";
     metricsCollector.gauge().register(
         TEST_GAUGE,
         Constants.Namespace.TEST,
         SUBSYSTEM_GAUGE_TEST,
-        "");
+        "test gauge help");
+  }
+
+  /**
+   * Tests correctness of gauge for a single node.
+   * Sets value of gauge several times, and evaluates
+   * only the last value successfully overwrites all
+   * preceding sets.
+   */
+  @Test
+  void singleNodeTest(){
+    UUID id = UUID.randomUUID();
+
+    long finalValue = 0;
+
+    // sets the gauge value several times
+    for (int i = 0; i < ITERATIONS; i++) {
+      int v = rand.nextInt(1000);
+      finalValue = v;
+      metricsCollector.gauge().set(TEST_GAUGE, id, v);
+    }
+
+    // only final value for gauge should be recorded and all interim values
+    // must be overwritten.
+    assertEquals(finalValue, metricsCollector.gauge().get(TEST_GAUGE, id));
+  }
+
+  @Test
+  void valueTest() {
+
+
+
     ArrayList<UUID> allId = new ArrayList<>();
     while (allId.size() != THREAD_CNT) {
       allId.add(UUID.randomUUID());
     }
     count = new CountDownLatch(THREAD_CNT);
 
-    // increment single entry
-    UUID id = UUID.randomUUID();
-    long tot = 0;
-    for (int i = 0; i < ITERATIONS; i++) {
-      int v = rand.nextInt(1000);
-      tot += v;
-      metricsCollector.gauge().inc(TEST_GAUGE, id, v);
-    }
-    assertEquals(tot, metricsCollector.gauge().get(TEST_GAUGE, id));
+
+
 
     for (UUID nodeId : allId) {
       new Thread(() -> threadTest(nodeId, ITERATIONS)).start();
@@ -60,9 +80,9 @@ class SimulatorGaugeTest {
       e.printStackTrace();
     }
 
-    tot = 0;
+    int tot = 0;
     for (UUID nodeId : allId) {
-      tot += metricsCollector.gauge().get("TestGauge", nodeId);
+      tot += metricsCollector.gauge().get(TEST_GAUGE, nodeId);
     }
     assertTrue(Math.abs(tot / (ITERATIONS * THREAD_CNT)) <= EPS);
   }
@@ -70,9 +90,9 @@ class SimulatorGaugeTest {
   void threadTest(UUID nodeId, int iterations) {
     while (iterations-- > 0) {
       if (rand.nextBoolean()) {
-        assertTrue(metricsCollector.gauge().inc("TestGauge", nodeId));
+        assertTrue(metricsCollector.gauge().inc(TEST_GAUGE, nodeId));
       } else {
-        assertTrue(metricsCollector.gauge().dec("TestGauge", nodeId));
+        assertTrue(metricsCollector.gauge().dec(TEST_GAUGE, nodeId));
       }
     }
     count.countDown();
