6c0b133275bd52a1252f0fdeacb7b58d14802b24
yhassanzadeh13 <yhassanzadeh13@ku.edu.tr>
2021-12-25 06:52:35 -0800

    adds java doc

--- src/main/java/simulator/Factory.java
+++ src/main/java/simulator/Factory.java
@@ -2,16 +2,28 @@ package simulator;
 
 import java.util.ArrayList;
 
+/**
+ * Factory supports creating several types of nodes each with a certain instances.
+ */
 public class Factory {
   private final ArrayList<Recipe> recipes;
   private int total;
 
+  /**
+   * Constructor of factory, creates an empty one.
+   */
   public Factory() {
     this.recipes = new ArrayList<>();
     this.total = 0;
   }
 
-  public void AddRecipe(Recipe r) {
+  /**
+   * Adds a recipe to this factory. A recipe contains a type of node as well
+   * as the total instance of that type that must be created by factory for simulation.
+   *
+   * @param r recipe for a new node.
+   */
+  public void addRecipe(Recipe r) {
     this.recipes.add(r);
     this.total += r.getTotal();
   }
