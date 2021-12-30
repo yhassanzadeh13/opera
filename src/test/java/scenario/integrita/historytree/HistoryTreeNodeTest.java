b5a3400488fdaad3a758208e6e77529f63f97209
Sanaz <staheri14@ku.edu.tr>
2021-12-30 14:38:28 -0800

    resolves linter issues

--- src/test/java/scenario/integrita/historytree/HistoryTreeNodeTest.java
+++ src/test/java/scenario/integrita/historytree/HistoryTreeNodeTest.java
@@ -1,42 +1,43 @@
 package scenario.integrita.historytree;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
-import org.junit.platform.engine.support.hierarchical.Node;
 
 public class HistoryTreeNodeTest {
 
   @Test
-  void testIsValid(){
-    assertFalse(NodeAddress.isValid(new NodeAddress(0,0)));
-    assertFalse(NodeAddress.isValid(new NodeAddress(1,-1)));
-    assertFalse(NodeAddress.isValid(new NodeAddress(4,3)));
-    assertFalse(NodeAddress.isValid(new NodeAddress(3,3)));
+  void testIsValid() {
+    assertFalse(NodeAddress.isValid(new NodeAddress(0, 0)));
+    assertFalse(NodeAddress.isValid(new NodeAddress(1, -1)));
+    assertFalse(NodeAddress.isValid(new NodeAddress(4, 3)));
+    assertFalse(NodeAddress.isValid(new NodeAddress(3, 3)));
 
-    assertTrue(NodeAddress.isValid(new NodeAddress(3,2)));
-    assertTrue(NodeAddress.isValid(new NodeAddress(4,2)));
-  }
-  @Test
-  void testIsFull(){
-    assertTrue(NodeAddress.isFull(new NodeAddress(1,0)));
-    assertTrue(NodeAddress.isFull(new NodeAddress(2,0)));
-    assertTrue(NodeAddress.isFull(new NodeAddress(2,1)));
-    assertTrue(NodeAddress.isFull(new NodeAddress(3,0)));
+    assertTrue(NodeAddress.isValid(new NodeAddress(3, 2)));
+    assertTrue(NodeAddress.isValid(new NodeAddress(4, 2)));
   }
 
   @Test
-  void testIsTemporary(){
-    assertTrue(NodeAddress.isTemporary(new NodeAddress(3,2)));
-    assertTrue(NodeAddress.isTemporary(new NodeAddress(3,1)));
+  void testIsFull() {
+    assertTrue(NodeAddress.isFull(new NodeAddress(1, 0)));
+    assertTrue(NodeAddress.isFull(new NodeAddress(2, 0)));
+    assertTrue(NodeAddress.isFull(new NodeAddress(2, 1)));
+    assertTrue(NodeAddress.isFull(new NodeAddress(3, 0)));
   }
 
   @Test
-  void testIsTreeDigest(){
-    assertTrue(NodeAddress.isTreeDigest(new NodeAddress(1,0)));
-    assertTrue(NodeAddress.isTreeDigest(new NodeAddress(2,1)));
-    assertTrue(NodeAddress.isTreeDigest(new NodeAddress(3,2)));
-    assertTrue(NodeAddress.isTreeDigest(new NodeAddress(4,2)));
+  void testIsTemporary() {
+    assertTrue(NodeAddress.isTemporary(new NodeAddress(3, 2)));
+    assertTrue(NodeAddress.isTemporary(new NodeAddress(3, 1)));
+  }
+
+  @Test
+  void testIsTreeDigest() {
+    assertTrue(NodeAddress.isTreeDigest(new NodeAddress(1, 0)));
+    assertTrue(NodeAddress.isTreeDigest(new NodeAddress(2, 1)));
+    assertTrue(NodeAddress.isTreeDigest(new NodeAddress(3, 2)));
+    assertTrue(NodeAddress.isTreeDigest(new NodeAddress(4, 2)));
 
   }
 }
