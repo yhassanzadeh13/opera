a5df03677782e9bbd2be1b5b25c4b379af0f692d
Sanaz <staheri14@ku.edu.tr>
2022-01-08 09:59:50 -0800

    defines equal and hashcode
    there are required since NodeAddress is a HashMap key

--- src/main/java/scenario/integrita/historytree/NodeAddress.java
+++ src/main/java/scenario/integrita/historytree/NodeAddress.java
@@ -19,6 +19,26 @@ public class NodeAddress {
     this.level = level;
   }
 
+  //Compare only account numbers
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    NodeAddress other = (NodeAddress) obj;
+    if ((position != other.position) || (level != other.level))
+      return false;
+    return true;
+  }
+
+  @Override
+  public int hashCode() {
+    return toLabel(this);
+  }
+
   /**
    * checks whether the address fields are valid.
    */
