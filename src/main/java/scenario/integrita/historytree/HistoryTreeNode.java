ba6f818237c36bbe72aba2bb9d83026c1d362870
Sanaz <staheri14@ku.edu.tr>
2022-01-08 10:00:12 -0800

    replaces string with operation type enum

--- src/main/java/scenario/integrita/historytree/HistoryTreeNode.java
+++ src/main/java/scenario/integrita/historytree/HistoryTreeNode.java
@@ -3,18 +3,29 @@ package scenario.integrita.historytree;
 
 import java.util.Arrays;
 
+import scenario.integrita.utils.OperationType;
+
 /**
  * implementation of a decentralized history tree as instructed in Integrita.
  */
 public class HistoryTreeNode {
   public NodeAddress addr;
-  public String op;
+  public OperationType op;
   public byte[] hash;
   public byte[] signature;
   /*
   the id of the owning user
    */
   public Integer userId;
+
+  public HistoryTreeNode(NodeAddress addr, OperationType op, Integer userId) {
+    this.addr = addr;
+    this.op = op;
+    this.userId = userId;
+  }
+
+  public HistoryTreeNode() {
+  }
 
   @Override
   public String toString() {
