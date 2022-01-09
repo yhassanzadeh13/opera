8127770ab0467a804991f41c838d7e9e1839b950
Sanaz <staheri14@ku.edu.tr>
2022-01-09 10:11:17 -0800

    an empty implementation of hash algorithm

--- src/main/java/scenario/integrita/hash/Hash.java
+++ src/main/java/scenario/integrita/hash/Hash.java
@@ -1,2 +1,16 @@
-package scenario.integrita.hash;public class Hash {
+package scenario.integrita.hash;
+
+import java.nio.charset.StandardCharsets;
+
+//import org.bouncycastle.jcajce.provider.digest.Keccak;
+//import org.bouncycastle.util.encoders.Hex;
+
+public class Hash {
+  public static String generateHash(String originalString) {
+//    Keccak.Digest256 digest256 = new Keccak.Digest256();
+//    byte[] hashedByteArray = digest256.digest(
+//            originalString.getBytes(StandardCharsets.UTF_8));
+//    return new String(Hex.encode(hashedByteArray));
+    return "";
+  }
 }
