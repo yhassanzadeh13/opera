c5163a586c0794ace4db1d767dc2139dfa0ee8ee
Sanaz <staheri14@ku.edu.tr>
2022-01-09 09:12:18 -0800

    implements the mocked signature unit

--- src/main/java/scenario/integrita/signature/Signature.java
+++ src/main/java/scenario/integrita/signature/Signature.java
@@ -1,4 +1,17 @@
 package scenario.integrita.signature;
 
 public class Signature {
+
+  public void init(){
+  // @TODO generate keys
+  }
+  public byte[] sing(byte[] msg) {
+    // @TODO sign using the generated keys
+    byte[] result = new byte[0];
+    return result;
+  }
+  public boolean verify(){
+    // @TODO verify using the generated keys
+    return true;
+  }
 }
