482582a5a1adfebb9d6e4256413b9008f4dbc7c7
Sanaz <staheri14@ku.edu.tr>
2022-01-09 10:22:49 -0800

    adds user class

---
+++ src/main/java/scenario/integrita/user/User.java
@@ -0,0 +1,16 @@
+package scenario.integrita.user;
+
+/**
+ * contains user-related information
+ */
+public class User {
+  public Integer id;
+  public byte[] verification_key;
+
+  public User(Integer id) {
+    this.id = id;
+  }
+
+  public User() {
+  }
+}
