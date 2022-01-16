6a6abee351b3e01558091b7e3ec318b6e2e11ba0
Sanaz <staheri14@ku.edu.tr>
2022-01-16 12:20:18 -0800

    fixes in-place hashmap update

--- src/main/java/scenario/integrita/database/HistoryTreeStore.java
+++ src/main/java/scenario/integrita/database/HistoryTreeStore.java
@@ -1,9 +1,6 @@
 package scenario.integrita.database;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 import scenario.integrita.historytree.HistoryTreeNode;
 import scenario.integrita.historytree.NodeAddress;
@@ -64,24 +61,28 @@ public class HistoryTreeStore implements Store {
   }
 
   /**
-   * erases all the past nodes whose `position` precede the position of the supplied `addr`
+   * erases all the past nodes whose `position` precede the position of the supplied `addr` exclusively
+   *
    * @param addr
    */
-  public void cleanDigests(NodeAddress addr){
+  public void cleanDigests(NodeAddress addr) {
     Set<NodeAddress> keySet = historyTreeNodes.keySet();
-    for ( NodeAddress k : keySet){
-      if (NodeAddress.isTreeDigest(k) && (k.position < addr.position) && (k.position != 1)){
-        historyTreeNodes.remove(k);
+
+    for (Iterator<Map.Entry<NodeAddress, HistoryTreeNode>> it = historyTreeNodes.entrySet().iterator(); it.hasNext(); ) {
+      Map.Entry<NodeAddress, HistoryTreeNode> entry = it.next();
+      if (NodeAddress.isTreeDigest(entry.getKey()) && (entry.getKey().position < addr.position) && (entry.getKey().position != 1)) {
+        it.remove();
       }
     }
   }
 
   /**
    * checks if nodeAddress belongs to HistoryTreeStore object
+   *
    * @param nodeAddress
    * @return
    */
-  public boolean contains(NodeAddress nodeAddress){
+  public boolean contains(NodeAddress nodeAddress) {
     boolean exists = historyTreeNodes.containsKey(nodeAddress);
     return exists;
   }
