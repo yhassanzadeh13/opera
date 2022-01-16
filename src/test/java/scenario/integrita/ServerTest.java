c5c344f69fb795f27bdd2a3050d0e260169896e1
Sanaz <staheri14@ku.edu.tr>
2022-01-16 12:50:59 -0800

    fixes formating problems

--- src/test/java/scenario/integrita/ServerTest.java
+++ src/test/java/scenario/integrita/ServerTest.java
@@ -1,36 +1,50 @@
 package scenario.integrita;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import groovy.lang.Tuple;
 import org.junit.jupiter.api.Test;
+import scenario.integrita.historytree.HistoryTreeNode;
+import scenario.integrita.historytree.NodeAddress;
+import scenario.integrita.user.User;
+import scenario.integrita.utils.OperationType;
+import scenario.integrita.utils.StatusCode;
 
 public class ServerTest {
 
   @Test
-  public void pushTestServerIndex(){
-    // create a history tree node whose index maps to the server's index
+  public void pushTestServerIndex() {
+    // set up a server
     Server s = new Server();
     s.index = 1;
     s.totalServers = 4;
+    s.db.insert(new User());
+    // create a history tree node whose index maps to the server's index
+    HistoryTreeNode historyTreeNode = new HistoryTreeNode(new NodeAddress(1, 0), OperationType.Insert, 1);
+    Tuple res = s.push(historyTreeNode);
+    assertTrue(res.get(0) == StatusCode.Accept);
+
 
 
     // create a history tree node whose index does not map to the server's index
   }
 
   @Test
-  public void pushTestLabelDistance(){
+  public void pushTestLabelDistance() {
     // set server's status
 
     // create a history tree node whose index has more than N difference from the server's status
   }
 
   @Test
-  public void pushTestTemporaryNodes(){
+  public void pushTestTemporaryNodes() {
     // create a temporary history tree node
     // push it to the server
     // the db should not change
   }
 
   @Test
-  public void pushTestInsertTreeDigest(){
+  public void pushTestInsertTreeDigest() {
     // populate a server with proper nodes of the history tree
     // create a tree digest
     // push it to the server
