230d44efa3e961c41025f4e756237ee60f057535
Sanaz <staheri14@ku.edu.tr>
2022-01-16 08:55:05 -0800

    mock implementation of signature

--- src/main/java/scenario/integrita/signature/Signature.java
+++ src/main/java/scenario/integrita/signature/Signature.java
@@ -1,16 +1,23 @@
 package scenario.integrita.signature;
 
+/**
+ * signature implementation.
+ */
 public class Signature {
 
-  public void init(){
-  // @TODO generate keys
+  public static byte[][] keyGen() {
+    // @TODO generate keys
+    byte[][] result =  new byte[2][];
+    return result;
   }
+
   public static byte[] sign(String msg, byte[] signature_key) {
     // @TODO sign using the generated keys
     byte[] result = new byte[0];
     return result;
   }
-  public static boolean verify(String msg, byte[] signature, byte[] verification_key){
+
+  public static boolean verify(String msg, byte[] signature, byte[] verification_key) {
     // @TODO verify using the generated keys
     return true;
   }
