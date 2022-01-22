be243e95ada35c5542ef5b9e5df5ebce413ac855
Sanaz <staheri14@ku.edu.tr>
2022-01-22 09:01:53 -0800

    adds tests for toLabel

---
+++ src/test/java/scenario/integrita/historytree/NodeAddressTest.java
@@ -0,0 +1,24 @@
+package scenario.integrita.historytree;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+
+public class NodeAddressTest {
+  @Test
+  public void toLabelTest() {
+    assertTrue(NodeAddress.toLabel(new NodeAddress(1, 0)) == 1);
+
+    assertTrue(NodeAddress.toLabel(new NodeAddress(2, 0)) == 2);
+    assertTrue(NodeAddress.toLabel(new NodeAddress(2, 1)) == 3);
+
+    assertTrue(NodeAddress.toLabel(new NodeAddress(3, 0)) == 4);
+    assertTrue(NodeAddress.toLabel(new NodeAddress(3, 1)) == 5);
+    assertTrue(NodeAddress.toLabel(new NodeAddress(3, 2)) == 6);
+
+    assertTrue(NodeAddress.toLabel(new NodeAddress(4, 0)) == 7);
+    assertTrue(NodeAddress.toLabel(new NodeAddress(4, 1)) == 8);
+    assertTrue(NodeAddress.toLabel(new NodeAddress(4, 2)) == 9);
+  }
+}
