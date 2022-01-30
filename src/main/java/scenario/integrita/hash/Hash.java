a7701d6b4178dbea3da83f0c7367404744c521b8
Sanaz <staheri14@ku.edu.tr>
2022-01-30 09:35:37 -0800

    resolves linter issues

--- src/main/java/scenario/integrita/hash/Hash.java
+++ src/main/java/scenario/integrita/hash/Hash.java
@@ -1,13 +1,8 @@
 package scenario.integrita.hash;
 
-import org.bouncycastle.util.encoders.Hex;
-
 /**
  * Hash implementation used in Merkle tree.
  */
 public class Hash {
-  /**
-   * hash function.
-   */
 
 }
