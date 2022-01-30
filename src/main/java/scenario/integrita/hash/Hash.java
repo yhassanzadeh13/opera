0ed1ed049d1d714f39c5c5865c553671711f671c
Sanaz <staheri14@ku.edu.tr>
2022-01-30 09:33:13 -0800

    deletes hash code

--- src/main/java/scenario/integrita/hash/Hash.java
+++ src/main/java/scenario/integrita/hash/Hash.java
@@ -1,8 +1,6 @@
 package scenario.integrita.hash;
 
-
-//import org.bouncycastle.jcajce.provider.digest.Keccak;
-//import org.bouncycastle.util.encoders.Hex;
+import org.bouncycastle.util.encoders.Hex;
 
 /**
  * Hash implementation used in Merkle tree.
@@ -11,11 +9,5 @@ public class Hash {
   /**
    * hash function.
    */
-  public static String generateHash(String originalString) {
-    //    Keccak.Digest256 digest256 = new Keccak.Digest256();
-    //    byte[] hashedByteArray = digest256.digest(
-    //            originalString.getBytes(StandardCharsets.UTF_8));
-    //    return new String(Hex.encode(hashedByteArray));
-    return "";
-  }
+
 }
