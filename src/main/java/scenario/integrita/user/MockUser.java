10135198bf4a3df78ddc452d63eff8d742f84049
Sanaz <staheri14@ku.edu.tr>
2022-03-10 10:14:02 -0800

    implements aggregate methods

--- src/main/java/scenario/integrita/user/MockUser.java
+++ src/main/java/scenario/integrita/user/MockUser.java
@@ -4,33 +4,45 @@ package scenario.integrita.user;
  * implementation of a mock user.
  */
 public class MockUser implements BaseUser {
+  static int readTotalTime;
+  static int writeTotalTime;
+  static int updateStatusTotalTime;
+
+  static int readCount;
+  static int writeCount;
+  static int updateStatusCount;
   @Override
   public int read() {
+
+    readCount ++;
     return 0;
   }
 
   @Override
   public int write() {
+
+    writeCount ++;
     return 0;
   }
 
   @Override
   public int updateStatus() {
+    updateStatusCount ++;
     return 0;
   }
 
   @Override
   public float getAvgReadTime() {
-    return 0;
+    return readTotalTime/readCount;
   }
 
   @Override
   public float getAvgWriteTime() {
-    return 0;
+    return writeTotalTime/writeCount;
   }
 
   @Override
   public float getAvgUpdateStatusTime() {
-    return 0;
+    return updateStatusTotalTime/updateStatusCount;
   }
 }
