f0328ecd1f992039a4baddab975a99387a00ad88
Sanaz <staheri14@ku.edu.tr>
2022-03-30 14:53:23 -0700

    adds benchmark class

---
+++ src/main/java/scenario/integrita/user/BenchMark.java
@@ -0,0 +1,10 @@
+package scenario.integrita.user;
+
+public class BenchMark {
+  public static int NetDelay;
+  public static int SignVerify;
+  public static int Hash;
+  public static int TotalServers;
+  public static int Fpl;
+
+}
