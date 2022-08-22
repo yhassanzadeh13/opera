a1c524212c5ce7284e32d1ba0b0b5c3b5740b74a
Yahya <yhassanzadeh13@ku.edu.tr>
2022-08-22 07:04:57 -0700

    supress spotbugs

--- src/main/java/network/javarmi/JavaRmiHost.java
+++ src/main/java/network/javarmi/JavaRmiHost.java
@@ -1,15 +1,18 @@
 package network.javarmi;
 
 import java.rmi.RemoteException;
+import java.rmi.server.RemoteObject;
 import java.rmi.server.UnicastRemoteObject;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import network.packets.Request;
 
 
 /**
  * Represents the Java RMI Service implementation.
  */
-public class JavaRmiHost implements network.javarmi.JavaRmiService {
+public class JavaRmiHost extends RemoteObject implements network.javarmi.JavaRmiService {
+  @SuppressFBWarnings(value = "SE_BAD_FIELD", justification = "it is meant to expose internal state of MiddleLayer")
   private final JavaRmiUnderlay underlay;
 
   public JavaRmiHost(JavaRmiUnderlay underlay) throws RemoteException {
