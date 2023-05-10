9143d45fa8b1ace9aeaab4b11b7534eb7738c198
Yahya <yhassanzadeh13@ku.edu.tr>
2023-05-10 07:05:09 -0700

    adds uniform distribution tests

--- src/test/java/utils/churn/UniformGeneratorTest.java
+++ src/test/java/utils/churn/UniformGeneratorTest.java
@@ -1,14 +1,14 @@
 package utils.churn;
 
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-
-import static org.junit.jupiter.api.Assertions.*;
-
+/**
+ * Test class for UniformGenerator class.
+ */
 class UniformGeneratorTest {
   private int min;
   private int max;
@@ -21,6 +21,10 @@ class UniformGeneratorTest {
     generator = new UniformGenerator(min, max);
   }
 
+  /**
+   * Test method that checks if the generated value is always within the specified range.
+   * This test is repeated 1000 times to ensure a sufficient number of samples are tested.
+   */
   @Test
   void next_alwaysReturnsValueInRange() {
     for (int i = 0; i < 1000; i++) {
@@ -30,9 +34,15 @@ class UniformGeneratorTest {
     }
   }
 
+
+  /**
+   * Test method that checks if the generated values are uniformly distributed within the specified range.
+   * It generates 10K samples and counts the occurrences of each value, comparing them to the expected count.
+   * An acceptable tolerance is defined to account for minor variations in counts.
+   */
   @Test
   void next_generatesUniformlyDistributedValues() {
-    int sampleSize = 1000;
+    int sampleSize = 10_000;
     int tolerance = (int) (sampleSize * 0.1);
     int[] counts = new int[max - min];
     for (int i = 0; i < sampleSize; i++) {
@@ -51,11 +61,11 @@ class UniformGeneratorTest {
   /**
    * This test is not guaranteed to pass, but it should pass most of the time. If it fails, it
    * should be run again. If it fails consistently, there is a problem with the generator.
-   *
+   * <p>
    * Evaluates the uniqueness of the generated values. The test passes if the number of unique
    * values is within the acceptable tolerance, i.e., it keeps generating random values
    * and tracks the number of duplicates. If the number of duplicates is within the acceptable
-   * tolerance, the test passes. 
+   * tolerance, the test passes.
    */
   @Test
   void next_generatesUniqueValues() {
@@ -68,6 +78,39 @@ class UniformGeneratorTest {
       assertTrue(counts.get(value) <= tolerance,
               "Value count should be within the acceptable tolerance");
     }
+  }
+
+  /**
+   * Test method that checks if the generated values have a mean and variance close to the expected
+   * mean and variance of a uniform distribution.
+   * It generates 10000 samples and computes the sample mean and variance, comparing them to the
+   * theoretical mean and variance of a uniform distribution.
+   * An acceptable tolerance is defined to account for minor variations.
+   */
+  @Test
+  void next_generatesValuesWithExpectedMeanAndVariance() {
+    int sampleSize = 10_000;
+    double tolerance = 0.01;
+
+    double expectedMean = (min + max) / 2.0;
+    double expectedVariance = Math.pow(max - min, 2) / 12.0;
+
+    double sum = 0;
+    double sumSquared = 0;
+
+    for (int i = 0; i < sampleSize; i++) {
+      int value = generator.next();
+      sum += value;
+      sumSquared += Math.pow(value, 2);
+    }
+
+    double sampleMean = sum / sampleSize;
+    double sampleVariance = (sumSquared - (Math.pow(sum, 2) / sampleSize)) / (sampleSize - 1);
+
+    assertTrue(Math.abs(((float) (expectedMean - sampleMean)) / expectedMean) < tolerance,
+            "Sample mean should be close to the expected mean of a uniform distribution");
+    assertTrue(Math.abs(((float) (expectedVariance - sampleVariance)) / expectedVariance) < tolerance,
+            "Sample variance should be close to the expected variance of a uniform distribution");
   }
 }
 
