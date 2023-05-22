9677b739f5449ada38080183dfd648e059d783dd
Yahya <yhassanzadeh13@ku.edu.tr>
2023-05-22 07:07:10 -0700

    fixes the tests

--- src/test/java/utils/churn/GaussianGeneratorTest.java
+++ src/test/java/utils/churn/GaussianGeneratorTest.java
@@ -7,8 +7,14 @@ import java.util.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+/**
+ * Test class for GaussianGenerator class.
+ */
 public class GaussianGeneratorTest {
 
+    /**
+     * Tests if the generated values are within the specified range. This test is repeated 10K times.
+     */
     @Test
     public void testGaussianGeneratorValuesInRange() {
         int mean = 50;
@@ -23,6 +29,9 @@ public class GaussianGeneratorTest {
         }
     }
 
+    /**
+     * Performs the 99.7, 95 and 68.2 test for the generated values. The test is performed for 1M samples.
+     */
     @Test
     public void testGaussianGeneratorDistribution() {
         int mean = 50;
@@ -43,74 +52,38 @@ public class GaussianGeneratorTest {
         }
         assertEquals(total, sampleSize, "Total count should be equal to sample size");
 
-//        // Check if the generated values follow a Gaussian distribution
-//        for (int i = -3; i <= 3; i++) {
-//            int rangeMin = mean + (i * std);
-//            int rangeMax = mean + ((i + 1) * std);
-//            int count = 0;
-//
-//            for (double key : histogram.keySet()) {
-//                if (key >= rangeMin && key < rangeMax) {
-//                    count += histogram.get(key);
-//                }
-//            }
-//
-//            double percentage = (double) count / sampleSize;
-//            System.out.println("i:" + i + " rangeMin: " + rangeMin + " rangeMax: " + rangeMax + " count: " + count + " percentage: " + percentage);
-//            // The correct percentages are based on the properties of the Gaussian distribution and the 68-95-99.7 rule (also known as the empirical rule). According to this rule, for a Gaussian distribution:
-//            //
-//            // About 68.27% of the values fall within 1 standard deviation (±1σ) of the mean.
-//            // About 95.45% of the values fall within 2 standard deviations (±2σ) of the mean.
-//            // About 99.73% of the values fall within 3 standard deviations (±3σ) of the mean.
-//            //
-//            // Based on the empirical rule, the correct percentage ranges for the test should be:
-//            //
-//            // For ranges -3std and 3std (the tails of the distribution), the test checks if the percentage is between 0% and (100% - 99.73%) / 2 = 0.135%.
-//            // For ranges -2std and 2std, the test checks if the percentage is between (99.73% - 95.45%) / 2 = 2.14% and (100% - 95.45%) / 2 = 2.275%.
-//            // For ranges -1std and 1std, the test checks if the percentage is between (95.45% - 68.27%) / 2 = 13.59% and (100% - 68.27%) / 2 = 15.865%.
-//            // For the range around the mean (0*std), the test checks if the percentage is between 68.27% / 2 = 34.135% and 100% / 2 = 50%.
-//            if (i == -3 || i == 3) {
-//                assertTrue(percentage >= 0.0 && percentage <= 0.0135, String.format("Generated values do not follow Gaussian distribution. i: %d, percentage: %f", i, percentage));
-//            } else if (i == -2 || i == 2) {
-//                assertTrue(percentage >= 0.0214 && percentage <= 0.02275, String.format("Generated values do not follow Gaussian distribution. i: %d, percentage: %f", i, percentage));
-//            } else if (i == -1 || i == 1) {
-//                assertTrue(percentage >= 0.1359 && percentage <= 0.15865, String.format("Generated values do not follow Gaussian distribution. i: %d, percentage: %f", i, percentage));
-//            } else {
-//                assertTrue(percentage >= 0.34135 && percentage <= 0.5, String.format("Generated values do not follow Gaussian distribution. i: %d, percentage: %f", i, percentage));
-//            }
-//        }
-
-        double maxTailPercentage = 0.0135;
-        double maxTwoStdPercentage = 0.02275;
-        double maxOneStdPercentage = 0.15865;
-        double minMeanPercentage = 0.34135;
-        double toleranceRatio = 2.0; //
-
         // Check if the generated values follow a Gaussian distribution
         for (int i = -3; i <= 3; i++) {
             int rangeMin = mean + (i * std);
             int rangeMax = mean + ((i + 1) * std);
-            double count = 0;
+            int count = 0;
 
-            List<Double> sortedKeys = new ArrayList<>(histogram.keySet());
-            Collections.sort(sortedKeys);
-            for (double key : sortedKeys) {
+            for (double key : histogram.keySet()) {
                 if (key >= rangeMin && key < rangeMax) {
                     count += histogram.get(key);
                 }
             }
 
-            double percentage = count / sampleSize;
+            double percentage = (double) count / sampleSize;
             System.out.println("i:" + i + " rangeMin: " + rangeMin + " rangeMax: " + rangeMax + " count: " + count + " percentage: " + percentage);
-
-            if (i == -3 || i == 3) {
-                assertTrue(percentage >= 0.0 && percentage <= maxTailPercentage * toleranceRatio, String.format("Generated values do not follow Gaussian distribution. i: %d, percentage: %f", i, percentage));
-            } else if (i == -2 || i == 2) {
-                assertTrue(percentage >= maxTailPercentage && percentage <= maxTwoStdPercentage, String.format("Generated values do not follow Gaussian distribution. i: %d, percentage: %f", i, percentage));
-            } else if (i == -1 || i == 1) {
-                assertTrue(percentage >= maxTwoStdPercentage && percentage <= maxOneStdPercentage, String.format("Generated values do not follow Gaussian distribution. i: %d, percentage: %f", i, percentage));
-            } else {
-                assertTrue(percentage >= minMeanPercentage && percentage <= 0.5, String.format("Generated values do not follow Gaussian distribution. i: %d, percentage: %f", i, percentage));
+            // The correct percentages are based on the properties of the Gaussian distribution and the 68-95-99.7 rule (also known as the empirical rule). According to this rule, for a Gaussian distribution:
+            //
+            // About 68.27% of the values fall within 1 standard deviation (±1σ) of the mean.
+            // About 95.45% of the values fall within 2 standard deviations (±2σ) of the mean.
+            // About 99.73% of the values fall within 3 standard deviations (±3σ) of the mean.
+            //
+            // Based on the empirical rule, the correct percentage ranges for the test should be:
+            // Within 1 standard deviation (±1σ) of the mean, check if around 68% of the values are generated in this range, i.e., 68.27% / 2 = 34.135% and 100% / 2 = 50%.
+            if (i == -1 || i == 0) {
+                assertTrue(percentage > 0.34 && percentage < 0.5, String.format("Generated values do not follow Gaussian distribution. i: %d, percentage: %f", i, percentage));
+            }
+            // Within 2 standard deviations (±2σ) of the mean, check if around 95% of the values are generated in this range, i.e., (95.45 - 68.27) / 2 = 13.5%.
+            else if (i == -2 || i == 1) {
+                assertTrue(percentage > 0.13 && percentage < 0.34, String.format("Generated values do not follow Gaussian distribution. i: %d, percentage: %f", i, percentage));
+            }
+            // Within 3 standard deviations (±3σ) of the mean, check if around 99% of the values are generated in this range, i.e., (99.73 - 95.45) / 2 = 2.14%.
+            else if (i == -3 || i == 2) {
+                assertTrue(percentage > 0.02 && percentage < 0.13, String.format("Generated values do not follow Gaussian distribution. i: %d, percentage: %f", i, percentage));
             }
         }
     }
