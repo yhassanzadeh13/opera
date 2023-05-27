c7bb346f7453b5641d5cd552e6c087a03ac0e0cd
Yahya <yhassanzadeh13@ku.edu.tr>
2023-05-27 06:44:50 -0700

    adds test for weibull distribution

--- src/test/java/utils/churn/WeibullGeneratorTest.java
+++ src/test/java/utils/churn/WeibullGeneratorTest.java
@@ -1,12 +1,21 @@
 package utils.churn;
 
+import org.apache.commons.math3.distribution.WeibullDistribution;
 import org.apache.commons.math3.stat.descriptive.DescriptiveStatistics;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+/**
+ * This is a class used to test the WeibullGenerator class. It tests the constructor of the WeibullGenerator class,
+ * tests the next() method for generated values within the range and tests the distribution of generated values.
+ */
 public class WeibullGeneratorTest {
+  /**
+   * Test the constructor of the WeibullGenerator class.
+   * The constructor should return a non-null WeibullGenerator instance.
+   */
   @Test
   public void testConstructor() {
     double min = 1;
@@ -18,6 +27,10 @@ public class WeibullGeneratorTest {
     assertNotNull(weibullGenerator, "WeibullGenerator should not be null");
   }
 
+  /**
+   * Test the next() method of the WeibullGenerator class.
+   * It creates an instance of the WeibullGenerator and checks if the next() method returns a value in the specified range.
+   */
   @Test
   public void testNextInRange() {
     double sample;
@@ -36,13 +49,18 @@ public class WeibullGeneratorTest {
     }
   }
 
+  /**
+   * Test if the next() method of the WeibullGenerator class generates values following the Weibull distribution.
+   * It creates an instance of the WeibullGenerator, generates a large sample and tests if the sample mean and variance
+   * are close to the expected mean and variance of the Weibull distribution.
+   */
   @Test
   public void testNextFollowsDistribution() {
     double min = 0;
     double max = Double.MAX_VALUE;
     double shapeParameter = 2;
     double scaleParameter = 3;
-    double TOLERANCE = 1E-1;
+    double TOLERANCE = 1E-2;
     WeibullGenerator weibullGenerator = new WeibullGenerator(min, max, shapeParameter, scaleParameter);
 
     int sampleSize = 1000000;
@@ -54,15 +72,12 @@ public class WeibullGeneratorTest {
 
     double sampleMean = stats.getMean();
     double sampleVariance = stats.getVariance();
-    double sampleSkewness = stats.getSkewness();
 
-    // Weibull distribution parameters
-    double expectedMean = scaleParameter * Math.exp(1.0 / shapeParameter);
-    double expectedVariance = Math.pow(scaleParameter, 2) * (0.0);
-    // double expectedSkewness = (Math.exp(3.0 / shapeParameter) - 3 * Math.exp(2.0 / shapeParameter) + 2) / Math.sqrt(0.0);
+    WeibullDistribution weibullDistribution = new WeibullDistribution(shapeParameter, scaleParameter);
+    double expectedMean = weibullDistribution.getNumericalMean();
+    double expectedVariance = weibullDistribution.getNumericalVariance();
 
     assertTrue(Math.abs(expectedMean - sampleMean) < TOLERANCE, String.format("Sample mean %f should be close to expected mean %f", sampleMean, expectedMean));
     assertTrue(Math.abs(expectedVariance - sampleVariance) < TOLERANCE, String.format("Sample variance %f should be close to expected variance %f", sampleVariance, expectedVariance));
-    // assertTrue(Math.abs(expectedSkewness - sampleSkewness) < TOLERANCE, String.format("Sample skewness %f should be close to expected skewness %f", sampleSkewness, expectedSkewness));
   }
 }
