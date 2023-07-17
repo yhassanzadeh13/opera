e783cc496fef09f981bc6c9204962b873a722679
Yahya <yhassanzadeh13@ku.edu.tr>
2023-07-17 07:39:07 -0700

    adds entity encoder

---
+++ src/test/java/network/encoder/serializable/EncoderTest.java
@@ -0,0 +1,61 @@
+package network.encoder.serializable;
+
+import network.packets.Event;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class EncoderTest {
+
+    private Encoder encoder;
+    private Event event;
+
+    @BeforeEach
+    void setUp() {
+        encoder = new Encoder();
+        event = new TestEvent("Test Event");
+    }
+
+    @Test
+    void encode() {
+        byte[] bytes = encoder.encode(event);
+        assertArrayEquals(bytes, encoder.encode(event));
+    }
+
+    @Test
+    void decode() {
+        byte[] bytes = encoder.encode(event);
+        assertEquals(event, encoder.decode(bytes));
+    }
+
+    @Test
+    void decodeShouldThrowExceptionForInvalidData() {
+        byte[] invalidData = new byte[]{0, 1, 2, 3};
+        assertThrows(IllegalStateException.class, () -> encoder.decode(invalidData));
+    }
+
+    static class TestEvent implements Event {
+        private String name;
+
+        TestEvent(String name) {
+            this.name = name;
+        }
+
+        // Need to override equals for the test to work.
+        @Override
+        public boolean equals(Object obj) {
+            if (this == obj) return true;
+            if (obj == null || getClass() != obj.getClass()) return false;
+            TestEvent testEvent = (TestEvent) obj;
+            return name.equals(testEvent.name);
+        }
+
+        @Override
+        public int hashCode() {
+            return name.hashCode();
+        }
+    }
+}
--- src/test/java/network/latency/LatencyGeneratorTest.java
+++ src/test/java/network/latency/LatencyGeneratorTest.java
@@ -1,4 +1,4 @@
-package network;
+package network.latency;
 
 import java.util.ArrayList;
 
